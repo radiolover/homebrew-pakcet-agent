d9d9440a843f6f9c487a6f6b0cd9da693de30715
vincent.te <vincent.ye@netis.com.cn>
2018-11-09 17:21:09 +0800

    add formula

---
+++ Formula/packet-agent.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class PacketAgent < Formula
+  desc "Netis Packet Agent Tools"
+  homepage ""
+  url "https://github.com/Netis/packet-agent/archive/v0.3.1.tar.gz"
+  sha256 "1e0fce9ecb5a698d046116c2ea1dbe3e80c1f455c17e8a54c94a7a993df65557"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    system "mkdir build && cmake"
+    system "cd build && make"
+    # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test packet-agent`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
